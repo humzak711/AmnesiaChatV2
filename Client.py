378e6ff3fc0bde0a92e7bfb00144940b0b0f0962
Humza <139543666+humzak711@users.noreply.github.com>
2024-02-27 00:08:47 +0000

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,7 +2,6 @@ import socket
 import threading
 import tkinter as tk
 from tkinter import ttk, scrolledtext, Entry, Button
-from modules.SecurityToolkit import encrypt_message, decrypt_message
 
 # Message server details
 SERVER_HOST: str = '127.0.0.1'
